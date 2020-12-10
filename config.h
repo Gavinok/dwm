d992b3f2a5d1761acb4419f407a7d9f54d6b86d8
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2020-12-10 12:08:25 -0700

    Increased font size

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 0;       /* vertical padding of bar */
 static const int sidepad            = 0;       /* horizontal padding of bar */
-static const char *fonts[]          = { "Liberation Mono:pixelsize=13:antialias=true:autohint=true" , "Font Awesome 5 Free:size=9" };
+static const char *fonts[]          = { "Liberation Mono:pixelsize=14:antialias=true:autohint=true" , "Font Awesome 5 Free:size=9" };
 static const char dmenufont[]       = "monospace-10";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
