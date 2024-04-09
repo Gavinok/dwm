57864c4ddd479aae1b75926f42c12238fc63b03d
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2024-04-08 17:32:40 -0700

    when fullscreening in monocle layout do not changing the bar shape

--- maximize.c
+++ maximize.c
@@ -1,33 +1,31 @@
 /* TODO: respect bar location */
+
 void
 max(const Arg *arg)
 {
-	unsigned int n;
-	Client *nbc;
 	if (!selmon->sel)
 		return;
-	/* count the number of clients  */
-	for (n = 0, nbc = nexttiled(selmon->clients); nbc; nbc = nexttiled(nbc->next), n++);
+
+	Client* c = selmon->sel;
+	Layout* l = selmon->lt[selmon->sellt]->arrange;
 	/* if the client is not fullscreen, alone, or monocle */
-	if (!selmon->sel->isfullscreen && (n != 1 || selmon->sel->isfloating || selmon->lt[selmon->sellt]->arrange == NULL)  && !(selmon->lt[selmon->sellt]->arrange == monocle)){
+	if (!c->isfullscreen && l != monocle && (nexttiled(c) || c->isfloating || l == NULL)){
 		/* if arg is 0 use monocle size if 1 use fullscreen */
 		if(selmon->showbar && (arg->i != 1)){
 			if (sp != 0) {
-				sp = 0;
-				vp = 0;
+				sp = vp = 0;
 				updatebarspace(selmon);
 			}
-			setfullscreen(selmon->sel, 2);
+			setfullscreen(c, 2);
 			return;
 		}
-		setfullscreen(selmon->sel, 1);
+		setfullscreen(c, 1);
 	}
 	else {
-		if (sp != sidepad) {
-			sp = sidepad;
-			vp = (topbar == 1) ? vertpad : - vertpad;
+		if ((sp != sidepad) && !(l == monocle)) {
+			sp = sidepad, vp = (topbar == 1) ? vertpad : - vertpad;
 			updatebarspace(selmon);
 		}
-		setfullscreen(selmon->sel, 0);
+		setfullscreen(c, 0);
 	}
 }
